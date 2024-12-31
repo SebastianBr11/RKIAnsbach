f95087b3eab4ab8f55c2e23671a1082271290826
Sebastian Breit <sebastian.breitan+github@gmail.com>
2024-12-31 10:12:51 +0100

    remove old project files

--- README.md
+++
@@ -1,4 +0,0 @@
-# RKI Ansbach App
-
-Dies war eine App, die ich zu Corona-Zeiten entwickelt habe, um schnell die aktuellen Inzidenzzahlen anzusehen.
-Die Verwendung von "RKI" bezieht sich dabei auf das Robert-Koch-Institut, diese App ist aber nicht damit assoziiert.
--- src/modules/ColorThemeModule.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModules } from 'react-native';
-const { ColorThemeModule } = NativeModules;
-interface ColorThemeInterface {
-  setTheme(color: String, light: boolean): void;
-}
-console.log(ColorThemeModule);
-export default ColorThemeModule as ColorThemeInterface;
