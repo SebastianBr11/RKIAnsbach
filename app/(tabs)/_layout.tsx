3874b2d41057f377b733f35428dfc722f4cf7812
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-01-13 17:37:45 +0100

    setup tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,41 @@
+import { HomeIcon } from '@/src/components/icons'
+import { useStyle } from '@/src/lib/styles'
 import { Tabs } from 'expo-router'
 
 export default function TabLayout() {
+  const { colors } = useStyle()
+
   return (
-    <Tabs>
-      <Tabs.Screen name="home" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.tabBarText,
+        tabBarActiveBackgroundColor: colors.text3,
+        tabBarInactiveTintColor: colors.text,
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <HomeIcon
+              fill={focused ? color : colors.text3}
+              height={size ? size : 24}
+              width={size ? size : 24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <CalendarIcon
+              fill={focused ? color : colors.text3}
+              height={size ? size : 24}
+              width={size ? size : 24}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
